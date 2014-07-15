924719b3aa4e834795454bc74f7c26b01b3d1e77
taiyeb zahir <taiyebzahir13@gmail.com>
2014-07-15 12:24:39 +0200

    Update gap_pattern.py

--- gap_pattern.py
+++ gap_pattern.py
@@ -24,9 +24,9 @@ trans_mat = numpy.array([[0,0,0,0,0,0,0,0,0,0,0,-4], #Hunchback
                        [0,-1,0,0,0,-1,0,0,0,0,1,1], #bicoid_receptor
                        [0,0,0,0,0,0,0,0,0,0,0,0], #ligand_bicoid
                        [0,0,0,0,0,0,0,0,0,0,0,0], #ligand_nanos
-		                   [0,0,0,0,0,0,0,0,0,0,-5,-5], #Giant
+		       [0,0,0,0,0,0,0,0,0,0,-5,-5], #Giant
                        [0,0,0,0,0,0,0,0,0,-3,0,0], #Kruppel
-                       [0,0,0,0,0,0,0,0,0,0,-2,0] #Knirps		
+                       [0,0,0,0,0,0,0,0,0,0,-4,0] #Knirps		
                        ])
 
 init_pop = generate_pop([(200, base2), (1200, base1), (200, base3)])
@@ -68,7 +68,7 @@ def run():
     x = Stops2(trans_mat, init_pop, grid.adj_mat, bound, secretion, reception, receptors, secr_amount=3, leak=0, max_con=10, max_dist=40, opencl=False)
     for i in range(300):
         x.step()
-        if i%5 == 0:
+        if i%4 == 0:
             print i
             draw_hex_grid(i, x.pop, grid, color_fun1, color_fun2, color_fun3, color_fun4, color_fun5)
 
